7d2d5a8ac1b4578d0acf993f95975b8522dd8008
AThooker <austintrent@icloud.com>
2020-11-09 18:06:28 -0500

    Add project files.

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _ctx = new ApplicationDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Customer Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Customer create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _ctx.Customers.Add(customer);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: Customer Delete
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _ctx.Customers.Find(id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //POST: Customer Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _ctx.Customers.Find(id);
+            _ctx.Customers.Remove(customer);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Customer Edit
+        public ActionResult Edit(int? id)
+        {
+            Customer customer = _ctx.Customers.Find(id);
+            return View(customer);
+        }
+
+        //POST: Customer Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if(!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _ctx.Entry(customer).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return RedirectToAction("Index")
+        }
+        //GET: Customer Details
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _ctx.Customers.Find(id);
+            return View(customer);
+        }
+    }
+}
---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> productList = _ctx.Products.ToList();
+            List<Product> orderList = productList.OrderByDescending(p => p.Name).ToList();
+            return View(orderList);
+        }
+
+        //GET: Product Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Product Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _ctx.Products.Add(product);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Product Delete
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _ctx.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+
+        //POST: Product Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _ctx.Products.Find(id);
+            _ctx.Products.Remove(product);
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Product Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _ctx.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        //POST: Product Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            _ctx.Entry(product).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //GET: Product Details
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _ctx.Products.Find(id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
